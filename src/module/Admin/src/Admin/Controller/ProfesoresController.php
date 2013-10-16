09fea36748fe83297646cf1ff6a3fa140cf70e59
Juan José Ramirez González <kraker_orion@ciencias.unam.mx>
2013-10-16 16:24:19 -0500

    Generando esqueleto de aplicación y agregando prototipos semánticos

---
+++ src/module/Admin/src/Admin/Controller/ProfesoresController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Admin\Controller;
+
+class ProfesoresController extends Zend\Mvc\Controller\AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+	public function todosAction()
+    {
+        return new ViewModel();
+    }
+	public function agregaAction()
+    {
+        return new ViewModel();
+    }
+	public function muestraAction()
+    {
+        return new ViewModel();
+    }
+    public function actualizaAction()
+    {
+        return new ViewModel();
+    }
+	public function borraAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
